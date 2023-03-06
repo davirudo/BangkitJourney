231d91592a3f1f106656296fcd1a2d402dd1030d
David Diamanta <dantadiamanta@gmail.com>
2023-03-07 05:03:23 +0700

    Final Boss : Improving UI

--- dndJourney/app/src/main/java/com/example/dndjourney/SplashScreen.kt
+++ dndJourney/app/src/main/java/com/example/dndjourney/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
         val action = supportActionBar
         action?.hide()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
