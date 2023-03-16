8083a45bf589b57580cddc108bd2a1f732c50924
David Diamanta <dantadiamanta@gmail.com>
2023-03-16 12:44:28 +0700

    add buildfesture -> viewBinding

--- ActionBar/app/src/main/java/com/example/actionbar/MainActivity.kt
+++ ActionBar/app/src/main/java/com/example/actionbar/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.example.actionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
