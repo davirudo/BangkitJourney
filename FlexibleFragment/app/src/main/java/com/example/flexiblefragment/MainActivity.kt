61125d17a1c42f070a98900fef424d3047a31f50
David Diamanta <dantadiamanta@gmail.com>
2023-03-18 12:15:20 +0700

    Android KTX

--- FlexibleFragment/app/src/main/java/com/example/flexiblefragment/MainActivity.kt
+++ FlexibleFragment/app/src/main/java/com/example/flexiblefragment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.flexiblefragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +15,10 @@ class MainActivity : AppCompatActivity() {
         val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
 
         if (fragment !is HomeFragment) {
-            Log.d("FlexibleFragment", "Fragment Name: ${HomeFragment::class.java.simpleName}")
-            mFragmentManager
-                .beginTransaction()
-                .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
-                .commit()
+            Log.d("FlexibleFragment", "Fragment Name: "+ HomeFragment::class.java.simpleName)
+            mFragmentManager.commit {
+                add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+            }
         }
-
-
     }
 }
