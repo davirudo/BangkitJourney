e892d16a0c677cfa070e91946337859a1a32b20b
David Diamanta <dantadiamanta@gmail.com>
2023-03-20 16:39:14 +0700

    implement Cuboid and ViewModel

--- UnitTest/app/src/main/java/com/example/unittest/MainActivity.kt
+++ UnitTest/app/src/main/java/com/example/unittest/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.unittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.unittest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        mainViewModel = MainViewModel(CuboidModel())
+        activityMainBinding.btnSave.setOnClickListener(this)
+        activityMainBinding.btnCalculateSurfaceArea.setOnClickListener(this)
+        activityMainBinding.btnCalculateCircumference.setOnClickListener(this)
+        activityMainBinding.btnCalculateVolume.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
