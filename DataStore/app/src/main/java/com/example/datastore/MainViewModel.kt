0d6547553bd82f5462b1c75f93dbe7f788f6e484
David Diamanta <dantadiamanta@gmail.com>
2023-03-22 23:28:06 +0700

    no 8 add logic

--- DataStore/app/src/main/java/com/example/datastore/MainViewModel.kt
+++ DataStore/app/src/main/java/com/example/datastore/MainViewModel.kt
@@ -1,5 +1,21 @@
 package com.example.datastore
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val pref: SettingPreferences): ViewModel() {
+
+    fun getThemeSetting(): LiveData<Boolean> {
+        return pref.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSetting(isDarkModeActive: Boolean) {
+        viewModelScope.launch {
+            pref.saveThemeSetting(isDarkModeActive)
+        }
+    }
 
 }
