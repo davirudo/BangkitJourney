1cc116ab65840f161d0ebf4941e1074eac4b2285
David Diamanta <dantadiamanta@gmail.com>
2023-03-24 02:56:10 +0700

    no 17 NotesApp

--- NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.notesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notesapp.adapter.NoteAdapter
+import com.example.notesapp.databinding.ActivityMainBinding
+import com.example.notesapp.entity.Note
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NoteAdapter
+
+    val resultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if(result.data != null) {
+            //akan dipanggil jika request codenya ADD
+            when (result.resultCode) {
+                NoteAddUpdateActivity.RESULT_ADD -> {
+                    val note = result.data?.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+                    adapter.addItem(note)
+                    binding.rvNotes.smoothScrollToPosition(adapter.itemCount - 1)
+                    showSnackbarMessage("Satu item berhasil ditambahkan")
+                }
+                NoteAddUpdateActivity.RESULT_UPDATE -> {
+                    val note = result.data?.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+                    val position = result?.data?.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0) as Int
+                    adapter.updateItem(position, note)
+                    binding.rvNotes.smoothScrollToPosition(position)
+                    showSnackbarMessage("Satu item berhasil diubah")
+                }
+                NoteAddUpdateActivity.RESULT_DELETE -> {
+                    val position = result?.data?.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0) as Int
+                    adapter.removeItem(position)
+                    showSnackbarMessage("Satu item berhasil dihapus")
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Notes"
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.setHasFixedSize(true)
+
+
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.rvNotes, message, Snackbar.LENGTH_SHORT).show()
     }
 }
