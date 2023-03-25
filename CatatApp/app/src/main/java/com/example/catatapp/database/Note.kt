a611254cc2f90feaea7affddf5d1446fcf91dc7d
David Diamanta <dantadiamanta@gmail.com>
2023-03-25 16:00:47 +0700

    made database

--- CatatApp/app/src/main/java/com/example/catatapp/database/Note.kt
+++ CatatApp/app/src/main/java/com/example/catatapp/database/Note.kt
@@ -1,2 +1,26 @@
 package com.example.catatapp.database
 
+import android.icu.text.CaseMap.Title
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity
+@Parcelize
+class Note (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+
+    @ColumnInfo(name = "title")
+    var title: String? = null,
+
+    @ColumnInfo(name = "description")
+    var description: String? = null,
+
+    @ColumnInfo(name = "date")
+    var date : String? = null
+
+        ) : Parcelable
