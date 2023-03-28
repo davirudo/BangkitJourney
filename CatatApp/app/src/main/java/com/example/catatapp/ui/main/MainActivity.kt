c9e1afd6beee4e14eae256f8c91f1839ad3c3e89
David Diamanta <dantadiamanta@gmail.com>
2023-03-28 21:13:49 +0700

    menghubungkan MainViewModel dengan MainActivity

--- CatatApp/app/src/main/java/com/example/catatapp/ui/main/MainActivity.kt
+++ CatatApp/app/src/main/java/com/example/catatapp/ui/main/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.catatapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.catatapp.R
+import com.example.catatapp.ViewModelFactory
 import com.example.catatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +20,23 @@ class MainActivity : AppCompatActivity() {
         _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this, { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        })
+
         adapter = NoteAdapter()
 
         binding?. rvNotes?.layoutManager = LinearLayoutManager(this)
         binding?.rvNotes?.setHasFixedSize(true)
         binding?.rvNotes?.adapter = adapter
+    }
+
+    private fun obtainViewModel(actiivity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(actiivity.application)
+        return ViewModelProvider(actiivity, factory).get(MainViewModel::class.java)
     }
 
     override fun onDestroy() {
