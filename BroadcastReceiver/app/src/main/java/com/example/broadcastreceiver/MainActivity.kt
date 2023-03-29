d601fa280d149e42d3d65d440ac51be6470b2088
David Diamanta <dantadiamanta@gmail.com>
2023-03-30 01:26:57 +0700

    add listener to button

--- BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.broadcastreceiver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.broadcastreceiver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnPermission?.setOnClickListener(this)
     }
+
+    override fun onClick(p0: View) {
+        when (p0.id) {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+
+
 }
