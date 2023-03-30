cf0ba6d8d482bbc933129d18824b61fd7b71d083
David Diamanta <dantadiamanta@gmail.com>
2023-03-31 01:22:11 +0700

    requestPermissionLauncer

--- BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.broadcastreceiver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.broadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -15,18 +17,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
-    }
 
-    override fun onClick(p0: View) {
-        when (p0.id) {
-
+        var requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Sms receiver permission ditolak",Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
 
         binding = null
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 
 
