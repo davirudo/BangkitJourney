84abd892daadf68284d728da8cc2f79edd72989a
David Diamanta <dantadiamanta@gmail.com>
2023-04-01 23:26:26 +0700

    Create FavUser.kt

---
+++ GithubUser/app/src/main/java/com/example/githubuser/database/FavUser.kt
@@ -0,0 +1,18 @@
+package com.example.githubuser.database
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity
+@Parcelize
+data class FavUser (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "username")
+    var username: String = "",
+
+    @ColumnInfo(name = "avatar_urL")
+    var avatar_url: String? = null,
+        ): Parcelable
