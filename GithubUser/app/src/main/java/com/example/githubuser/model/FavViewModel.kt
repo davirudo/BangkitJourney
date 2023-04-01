134dd24ee2aec2bed3c6e2416b53955f41e8eb7b
David Diamanta <dantadiamanta@gmail.com>
2023-04-02 00:45:07 +0700

    add ViewModel fav

--- GithubUser/app/src/main/java/com/example/githubuser/model/FavViewModel.kt
+++ GithubUser/app/src/main/java/com/example/githubuser/model/FavViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.githubuser.model
 
-class FavViewModel {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.githubuser.database.Fav
+import com.example.githubuser.repository.FavRepository
+
+class FavViewModel(application: Application): ViewModel() {
+
+    private val mFavRepository: FavRepository = FavRepository(application)
+
+    fun getAllFav(): LiveData<List<Fav>> = mFavRepository.getAllFav()
+
 }
