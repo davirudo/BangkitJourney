a5deca413e40f81f9ecd4754d86c3e97a364a762
David Diamanta <dantadiamanta@gmail.com>
2023-04-02 19:46:33 +0700

    made ViewModelFactory

--- GithubUser/app/src/main/java/com/example/githubuser/model/ViewModelFactory.kt
+++ GithubUser/app/src/main/java/com/example/githubuser/model/ViewModelFactory.kt
@@ -1,4 +1,34 @@
 package com.example.githubuser.model
 
-class ViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory private constructor(private val mApplication: Application) : ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(application)
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(mApplication) as T
+        } else if (modelClass.isAssignableFrom(FavViewModel::class.java)) {
+            return FavViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        return super.create(modelClass)
+    }
 }
