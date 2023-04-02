4eb542fbc4c42353272b589ff91f945a1f1e608b
David Diamanta <dantadiamanta@gmail.com>
2023-04-02 19:48:02 +0700

    implement Fav list

--- GithubUser/app/src/main/java/com/example/githubuser/ui/FavActivity.kt
+++ GithubUser/app/src/main/java/com/example/githubuser/ui/FavActivity.kt
@@ -2,11 +2,51 @@ package com.example.githubuser.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.githubuser.ItemsItem
 import com.example.githubuser.R
+import com.example.githubuser.adapter.UserAdapter
+import com.example.githubuser.database.Fav
+import com.example.githubuser.databinding.ActivityFavBinding
+import com.example.githubuser.model.FavViewModel
+import com.example.githubuser.model.ViewModelFactory
 
 class FavActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFavBinding
+    private lateinit var favViewModel: FavViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fav)
+        binding = ActivityFavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvFav.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+
+        binding.rvFav.addItemDecoration(itemDecoration)
+
+        val favViewModel = getFavViewModel(this@FavActivity)
+        favViewModel.getAllFav().observe(this) { favList ->
+            if (favList != null) {
+                val items = arrayListOf<ItemsItem>()
+                for (fav in favList) {
+                    val item = ItemsItem( login = fav.username,
+                        avatarUrl = fav.avatar_url.toString(),
+                    )
+                    items.add(item)
+                }
+                binding.rvFav.adapter = UserAdapter(items)
+            }
+        }
+    }
+
+    private fun getFavViewModel(activity: AppCompatActivity): FavViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory)[FavViewModel::class.java]
     }
 }
