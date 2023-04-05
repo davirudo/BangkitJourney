5743ce682ec24dc75bd7395f13b5a1d6fa34949e
David Diamanta <dantadiamanta@gmail.com>
2023-04-05 17:32:22 +0700

    no 12 Custom Button dan EditText

---
+++ CostumView/app/src/main/java/com/example/costumview/MyEditText.kt
@@ -0,0 +1,86 @@
+package com.example.costumview
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+
+class MyEditText : AppCompatEditText, View.OnTouchListener {
+
+    private lateinit var clearButtonImage: Drawable
+
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
+
+    private fun init() {
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_clear) as Drawable
+        setOnTouchListener(this)
+    }
+
+    private fun showClearButton() {
+    setButtonDrawable(endOfTheText = clearButtonImage)
+    }
+
+    private fun hideClearButton() {
+    setButtonDrawable()
+    }
+
+    private fun setButtonDrawable(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(startOfTheText, topOfTheText, endOfTheText, bottomOfTheText)
+    }
+
+    override fun onTouch(v : View?, event: MotionEvent): Boolean {
+        if(compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when(event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_clear) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_clear) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else return false
+        }
+        return false
+    }
+}
