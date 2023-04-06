08ff7aaf030c03729a686975c301aa62d25d0e17
David Diamanta <dantadiamanta@gmail.com>
2023-04-06 23:13:52 +0700

    add setMyButtonEnable

--- CostumView/app/src/main/java/com/example/costumview/MainActivity.kt
+++ CostumView/app/src/main/java/com/example/costumview/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.myButton)
+        myEditText = findViewById(R.id.myEditText)
+
+        setMyButton()
+    }
+
+    private fun setMyButton() {
+        val result = myEditText.text
+        myButton.isEnabled = myEditText.text.toString().isNotEmpty()
     }
 }
