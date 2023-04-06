e12c3981054c26ef18ed073b159e8987db8b2ed5
David Diamanta <dantadiamanta@gmail.com>
2023-04-07 01:05:39 +0700

    Create NumberGenerator.kt

---
+++ Widgets/app/src/main/java/com/example/widgets/NumberGenerator.kt
@@ -0,0 +1,12 @@
+package com.example.widgets
+
+import java.util.*
+
+internal object NumberGenerator {
+
+    fun generate(max: Int): Int {
+        val random = Random()
+        return random.nextInt(max)
+    }
+
+}
