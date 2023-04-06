6ca19c642cd6c28c9ef5584a3eafe4895aa706e8
David Diamanta <dantadiamanta@gmail.com>
2023-04-06 23:17:04 +0700

    terdapat input, MyButton akan enable

--- CostumView/app/src/main/java/com/example/costumview/MainActivity.kt
+++ CostumView/app/src/main/java/com/example/costumview/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.costumview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,24 @@ class MainActivity : AppCompatActivity() {
         myEditText = findViewById(R.id.myEditText)
 
         setMyButton()
+
+        myEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+                //
+            }
+
+            override fun onTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+                setMyButton()
+            }
+
+            override fun afterTextChanged(p0: Editable) {
+                //
+            }
+        })
+
+        myButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, myEditText.text, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setMyButton() {
