c0ecdee057c3b0df093daad47bc88a966273c14d
David Diamanta <dantadiamanta@gmail.com>
2023-04-08 01:18:37 +0700

    getCount and ViewAt

--- StackWidget/app/src/main/java/com/example/stackwidget/StackRemoteViewsFactory.kt
+++ StackWidget/app/src/main/java/com/example/stackwidget/StackRemoteViewsFactory.kt
@@ -1,10 +1,12 @@
 package com.example.stackwidget
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import androidx.core.os.bundleOf
 
 internal class StackRemoteViewsFactory(private val mContext: Context) : RemoteViewsService.RemoteViewsFactory {
 
@@ -25,12 +27,20 @@ internal class StackRemoteViewsFactory(private val mContext: Context) : RemoteVi
         TODO("Not yet implemented")
     }
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getCount(): Int = mWidgetItems.size
 
     override fun getViewAt(p0: Int): RemoteViews {
-        TODO("Not yet implemented")
+        val rv = RemoteViews(mContext.packageName, R.layout.widget_item)
+        rv.setImageViewBitmap(R.id.imageView, mWidgetItems[p0])
+
+        val extras = bundleOf(
+            ImageBannerWidget.EXTRA_ITEM to p0
+        )
+        val fillInIntent = Intent()
+        fillInIntent.putExtras(extras)
+
+        rv.setOnClickFillInIntent(R.id.imageView, fillInIntent)
+        return rv
     }
 
     override fun getLoadingView(): RemoteViews {
