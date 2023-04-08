f27cf7b32c3eee2e0277b5c1144dc1aac71f31ed
David Diamanta <dantadiamanta@gmail.com>
2023-04-08 14:50:01 +0700

    Update StackWidgetService.kt

--- StackWidget/app/src/main/java/com/example/stackwidget/StackWidgetService.kt
+++ StackWidget/app/src/main/java/com/example/stackwidget/StackWidgetService.kt
@@ -1,4 +1,9 @@
 package com.example.stackwidget
 
-class StackWidgetService {
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory = StackRemoteViewsFactory(this.applicationContext)
 }
