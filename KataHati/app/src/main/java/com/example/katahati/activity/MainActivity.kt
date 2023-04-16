2c1dfffcd8755d576e50ce217f7c426100916ffe
David Diamanta <dantadiamanta@gmail.com>
2023-04-17 02:46:09 +0700

    implement bottom nav

--- KataHati/app/src/main/java/com/example/katahati/activity/MainActivity.kt
+++ KataHati/app/src/main/java/com/example/katahati/activity/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.katahati.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.katahati.R
+import com.example.katahati.databinding.ActivityLoginBinding
+import com.example.katahati.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNav
+        val navController = binding.navFragment
 
 
     }
