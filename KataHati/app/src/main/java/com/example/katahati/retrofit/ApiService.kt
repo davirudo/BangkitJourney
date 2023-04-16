6114c1274b8caa66038a7f6563efd89f1994fc21
David Diamanta <dantadiamanta@gmail.com>
2023-04-17 00:22:59 +0700

    change part to field

--- KataHati/app/src/main/java/com/example/katahati/retrofit/ApiService.kt
+++ KataHati/app/src/main/java/com/example/katahati/retrofit/ApiService.kt
@@ -2,16 +2,13 @@ package com.example.katahati.retrofit
 
 import com.example.katahati.response.LoginResponse
 import retrofit2.Call
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
 interface ApiService {
     @FormUrlEncoded
     @POST("v1/login")
     fun login(
-        @Part email: String,
-        @Part password: String
+        @Field("email") email: String,
+        @Field("password") password: String
     ): Call<LoginResponse>
 }
