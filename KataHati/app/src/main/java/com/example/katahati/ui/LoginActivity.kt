7b77816df6822402978ccd0e1f7a8847b076694d
David Diamanta <dantadiamanta@gmail.com>
2023-04-16 23:05:11 +0700

    login succes response

--- KataHati/app/src/main/java/com/example/katahati/ui/LoginActivity.kt
+++ KataHati/app/src/main/java/com/example/katahati/ui/LoginActivity.kt
@@ -2,11 +2,47 @@ package com.example.katahati.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.katahati.R
+import com.example.katahati.databinding.ActivityLoginBinding
+import com.example.katahati.response.LoginResponse
+import com.example.katahati.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Objects
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private val api by lazy { ApiConfig().getApiService() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val edEmail = binding.edLoginEmail
+        val edPassword = binding.edLoginPassword
+
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(this, "Login diproses", Toast.LENGTH_SHORT).show()
+
+            api.login(edEmail.text.toString(), edPassword.text.toString())
+                .enqueue(object : Callback<LoginResponse> {
+                    override fun onResponse(
+                        call: Call<LoginResponse>,
+                        response: Response<LoginResponse>
+                    ) {
+                        Log.e("LoginResponse", response.toString())
+                    }
+
+                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                        Log.e("LoginError", t.toString())
+                    }
+
+                })
+        }
     }
 }
