9b602ea8beb74263013276f8db8376a2a9efb999
David Diamanta <dantadiamanta@gmail.com>
2023-04-17 12:00:14 +0700

    logouit success

--- KataHati/app/src/main/java/com/example/katahati/fragment/SettingFragment.kt
+++ KataHati/app/src/main/java/com/example/katahati/fragment/SettingFragment.kt
@@ -1,19 +1,35 @@
 package com.example.katahati.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.katahati.R
+import com.example.katahati.activity.LoginActivity
+import com.example.katahati.activity.LoginActivity.Companion.sessionManager
 
 class SettingFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+        val btnLogout = view.findViewById<Button>(R.id.buttonLogout)
+
+        btnLogout.setOnClickListener {
+            sessionManager.clearSession()
+
+            val moveIntent = Intent(activity, LoginActivity::class.java)
+            startActivity(moveIntent)
+            activity?.finish()
+        }
+
+        return view
     }
 }
