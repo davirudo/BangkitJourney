2508aee0e45824f48fcc423fa3b52b0af2a379bf
David Diamanta <dantadiamanta@gmail.com>
2023-04-18 01:27:17 +0700

    listStory and isLoading

--- KataHati/app/src/main/java/com/example/katahati/model/storiesViewModel.kt
+++ KataHati/app/src/main/java/com/example/katahati/model/storiesViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.katahati.model
 
-class storiesViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.katahati.response.ListStoryItem
 
+class storiesViewModel() : ViewModel() {
 
+    private val _listStory = MutableLiveData<List<ListStoryItem>>()
+    val listStory: MutableLiveData<List<ListStoryItem>> = _listStory
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: MutableLiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "StoriesFragment"
+    }
+
+    init {
+        setUserData(listOf())
+    }
+
+    fun setUserData(data: List<ListStoryItem>) {
+        _isLoading.value = true
+        _listStory.value = data
+    }
 }
