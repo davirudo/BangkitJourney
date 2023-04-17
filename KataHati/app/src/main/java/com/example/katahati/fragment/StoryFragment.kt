88f4c40af8d098c9c4654376a3bb637eff6e0bfc
David Diamanta <dantadiamanta@gmail.com>
2023-04-18 01:54:17 +0700

    trying to implement the viewmodel

--- KataHati/app/src/main/java/com/example/katahati/fragment/StoryFragment.kt
+++ KataHati/app/src/main/java/com/example/katahati/fragment/StoryFragment.kt
@@ -5,15 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.katahati.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.katahati.adapter.StoriesAdapter
+import com.example.katahati.databinding.FragmentStoryBinding
+import com.example.katahati.model.StoriesViewModel
+import com.example.katahati.response.ListStoryItem
 
 class StoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentStoryBinding
+    private val storiesViewModel: StoriesViewModel by lazy {
+        ViewModelProvider(this)[StoriesViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_story, container, false)
+    ): View {
+        binding = FragmentStoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        storiesViewModel.listStory.observe(viewLifecycleOwner) {
+            setUserData(it)
+        }
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.rvStory.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(requireContext(), layoutManager.orientation)
+        binding.rvStory.addItemDecoration(itemDecoration)
+
+        storiesViewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+    }
+
+    private fun setUserData(data: List<ListStoryItem>) {
+        val adapter = StoriesAdapter(data)
+        binding.rvStory.adapter = adapter
+        }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
 }
