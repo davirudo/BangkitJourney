119e1eab318940bdcae71a40880601f3de0bde32
David Diamanta <dantadiamanta@gmail.com>
2023-04-19 00:47:21 +0700

    stories API

--- KataHati/app/src/main/java/com/example/katahati/retrofit/ApiService.kt
+++ KataHati/app/src/main/java/com/example/katahati/retrofit/ApiService.kt
@@ -1,14 +1,22 @@
 package com.example.katahati.retrofit
 
 import com.example.katahati.response.LoginResponse
+import com.example.katahati.response.StoriesResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiService {
     @FormUrlEncoded
-    @POST("v1/login")
+    @POST("login")
     fun login(
         @Field("email") email: String,
         @Field("password") password: String
     ): Call<LoginResponse>
+
+    @GET("stories")
+    fun getAllStories(
+        @Header("Authorization") token: String,
+        @Query("size") size: Int? = 15,
+    ): Call<StoriesResponse>
 }
