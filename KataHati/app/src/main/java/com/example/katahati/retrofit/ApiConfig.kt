75d68fd0cefbf2fc7f6e86f1d0a7b55db248e9da
David Diamanta <dantadiamanta@gmail.com>
2023-04-18 14:49:30 +0700

    Update ApiConfig.kt

--- KataHati/app/src/main/java/com/example/katahati/retrofit/ApiConfig.kt
+++ KataHati/app/src/main/java/com/example/katahati/retrofit/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.example.katahati.retrofit
 
+import androidx.viewbinding.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,9 +9,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
 
     companion object {
+
         fun getApiService(): ApiService {
-            val loggingInterceptor =
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()
