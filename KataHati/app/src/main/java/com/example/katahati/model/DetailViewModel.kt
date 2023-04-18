21def3d9c297f75fa0a532b8e7d32ab117dc7668
David Diamanta <dantadiamanta@gmail.com>
2023-04-19 05:11:23 +0700

    detailViewModel logic

--- KataHati/app/src/main/java/com/example/katahati/model/DetailViewModel.kt
+++ KataHati/app/src/main/java/com/example/katahati/model/DetailViewModel.kt
@@ -1,4 +1,56 @@
 package com.example.katahati.model
 
-class DetailViewModel {
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.katahati.response.DetailResponse
+import com.example.katahati.response.Story
+import com.example.katahati.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DetailViewModel : ViewModel() {
+
+    private val _detailUser = MutableLiveData<DetailResponse>()
+    val detailUser: MutableLiveData<DetailResponse> = _detailUser
+
+    private  val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: MutableLiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "DetailActivity"
+    }
+
+    fun getDetailUser(token : String, id : String = "") {
+        _isLoading.value = true
+
+        val client = ApiConfig.getApiService().getDetail(token, id)
+        client.enqueue(object : Callback<DetailResponse> {
+            override fun onResponse(
+                call: Call<DetailResponse>,
+                response: Response<DetailResponse>
+            ) {
+                _isLoading.value = false
+                if(response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _detailUser.value = response.body()
+                    }
+                } else {
+                    Log.e(TAG, "OnResponse ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<DetailResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure : ${t.message.toString()}")
+            }
+
+
+        })
+    }
+
+
 }
