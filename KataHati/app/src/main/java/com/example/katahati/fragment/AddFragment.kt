04a79aecf9721bf2487218a04ceb2ba93b628916
David Diamanta <dantadiamanta@gmail.com>
2023-04-19 12:14:47 +0700

    ui logic

--- KataHati/app/src/main/java/com/example/katahati/fragment/AddFragment.kt
+++ KataHati/app/src/main/java/com/example/katahati/fragment/AddFragment.kt
@@ -6,15 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.katahati.R
+import com.example.katahati.databinding.FragmentAddBinding
+import com.example.katahati.databinding.FragmentStoryBinding
 
 class AddFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddBinding
+
+    var btnVisible = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+    ): View {
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ivPhoto = binding.ivPhoto
+        val ivCamera = binding.ivCamera
+        val ivFile = binding.ivFile
+
+        ivPhoto.setOnClickListener {
+            if (!btnVisible) {
+                ivCamera.visibility = View.VISIBLE
+                ivFile.visibility = View.VISIBLE
+                btnVisible = true
+            } else {
+                ivCamera.visibility = View.GONE
+                ivFile.visibility = View.GONE
+                btnVisible = false
+            }
+        }
+
+
+    }
+
+
 
 }
