f57788dd42fc356cad4dbca289bbf66e2916e96a
David Diamanta <dantadiamanta@gmail.com>
2023-04-22 02:56:05 +0700

    made emailEditText

---
+++ KataHati/app/src/main/java/com/example/katahati/utils/EmailEditText.kt
@@ -0,0 +1,51 @@
+package com.example.katahati.utils
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class EmailEditText : AppCompatEditText, View.OnTouchListener {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Email"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(p0).matches()) {
+                    error = "email address"
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable) {}
+        })
+    }
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        return false
+    }
+}
