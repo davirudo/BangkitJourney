7724115db0e7c16b1cd53110f3d62863ef91275d
David Diamanta <dantadiamanta@gmail.com>
2023-04-28 23:36:05 +0700

    apiservice parameter

--- LatihanPaging3/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuotePagingSource.kt
+++ LatihanPaging3/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuotePagingSource.kt
@@ -1,4 +1,17 @@
 package com.dicoding.myunlimitedquotes.data
 
-class QuotePagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.dicoding.myunlimitedquotes.network.ApiService
+import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
+
+class QuotePagingSource(private val apiService: ApiService) : PagingSource<Int, QuoteResponseItem>() {
+    override fun getRefreshKey(state: PagingState<Int, QuoteResponseItem>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, QuoteResponseItem> {
+        TODO("Not yet implemented")
+    }
+
 }
