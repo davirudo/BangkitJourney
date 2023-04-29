38872166487020d59ff8608445a4fc6f80041239
David Diamanta <dantadiamanta@gmail.com>
2023-04-29 18:31:50 +0700

    remoteMediator

--- LatihanPaging3RemoteMediator/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRemoteMediator.kt
+++ LatihanPaging3RemoteMediator/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRemoteMediator.kt
@@ -4,22 +4,41 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import com.dicoding.myunlimitedquotes.database.QuoteDatabase
 import com.dicoding.myunlimitedquotes.network.ApiService
 import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
 
 @OptIn(ExperimentalPagingApi::class)
-class QuoteRemoteMediator (
-
-    private val databse: QuoteDatabase,
+class QuoteRemoteMediator(
+    private val database: QuoteDatabase,
     private val apiService: ApiService
-        ) : RemoteMediator<Int, QuoteResponseItem>() {
-    
+) : RemoteMediator<Int, QuoteResponseItem>() {
+
+    private companion object {
+        const val INITIAL_PAGE_INDEX = 1
+    }
+    override suspend fun initialize(): InitializeAction {
+        return InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
+
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, QuoteResponseItem>
     ): MediatorResult {
-        TODO("Not yet implemented")
+        val page = INITIAL_PAGE_INDEX
+        try {
+            val responseData = apiService.getQuote(page, state.config.pageSize)
+            val endOfPaginationReached = responseData.isEmpty()
+            database.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    database.quoteDao().deleteAll()
+                }
+                database.quoteDao().insertQuote(responseData)
+            }
+            return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
+        } catch (exception: Exception) {
+            return MediatorResult.Error(exception)
+        }
     }
-
 }
