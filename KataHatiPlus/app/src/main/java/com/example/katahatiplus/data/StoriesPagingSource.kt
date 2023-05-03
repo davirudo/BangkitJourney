65318a2ccddf0bff60544e0886f79fea0b797524
David Diamanta <dantadiamanta@gmail.com>
2023-05-03 22:55:49 +0700

    impelemnt pagingsource

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/data/StoriesPagingSource.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/data/StoriesPagingSource.kt
@@ -1,7 +1,18 @@
 package com.example.katahatiplus.data
 
-class StoriesPagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.katahatiplus.response.ListStoryItem
+import com.example.katahatiplus.retrofit.ApiService
 
+class StoriesPagingSource(private val apiService: ApiService) : PagingSource<Int, ListStoryItem>() {
+    override fun getRefreshKey(state: PagingState<Int, ListStoryItem>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ListStoryItem> {
+        TODO("Not yet implemented")
+    }
 
 
 }
