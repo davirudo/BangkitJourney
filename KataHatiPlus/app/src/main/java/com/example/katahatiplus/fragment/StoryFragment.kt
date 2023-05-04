453507e9d32246cb89214181001162c4d0ed98b3
David Diamanta <dantadiamanta@gmail.com>
2023-05-05 00:56:33 +0700

    lanjut besok

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/fragment/StoryFragment.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/fragment/StoryFragment.kt
@@ -6,21 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.katahatiplus.activity.LoginActivity
+import com.example.katahatiplus.adapter.LoadingStateAdapter
 import com.example.katahatiplus.adapter.StoriesAdapter
+import com.example.katahatiplus.adapter.StoriesAdapterPaging
 import com.example.katahatiplus.databinding.FragmentStoryBinding
 import com.example.katahatiplus.model.StoriesViewModel
+import com.example.katahatiplus.model.StoriesViewModelPaging
+import com.example.katahatiplus.model.ViewModelFactory
 import com.example.katahatiplus.response.ListStoryItem
 import com.example.katahatiplus.utils.SessionManager
 
 class StoryFragment : Fragment() {
 
     private lateinit var binding: FragmentStoryBinding
-    private val storiesViewModel: StoriesViewModel by lazy {
-        ViewModelProvider(this)[StoriesViewModel::class.java]
+    private val storiesViewModel: StoriesViewModelPaging by viewModels {
+        ViewModelFactory(requireContext())
     }
 
     override fun onCreateView(
@@ -36,31 +41,46 @@ class StoryFragment : Fragment() {
 
         sessionManager = SessionManager(requireContext())
         val token = LoginActivity.sessionManager.getString("TOKEN")
-        storiesViewModel.getAllStories(token.toString())
-
-        storiesViewModel.listStory.observe(viewLifecycleOwner) {
-            getAllStories(it)
-        }
-
-        storiesViewModel.isLoading.observe(viewLifecycleOwner) {
-            showLoading(it)
-        }
+        storiesViewModel.getStories(token.toString())
+//        storiesViewModel.getAllStories(token.toString())
+//
+//        storiesViewModel.listStory.observe(viewLifecycleOwner) {
+//            getAllStories(it)
+//        }
+//
+//        storiesViewModel.isLoading.observe(viewLifecycleOwner) {
+//            showLoading(it)
+//        }
 
         val layoutManager = LinearLayoutManager(requireContext())
         binding.rvStory.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(requireContext(), layoutManager.orientation)
         binding.rvStory.addItemDecoration(itemDecoration)
 
+        getData(token.toString())
+
     }
 
-    private fun getAllStories(data: List<ListStoryItem>) {
-        val adapter = StoriesAdapter(data)
-        binding.rvStory.adapter = adapter
+    private fun getData(token: String) {
+        val adapter = StoriesAdapterPaging()
+        binding.rvStory.adapter = adapter.withLoadStateFooter(
+            footer = LoadingStateAdapter {
+                adapter.retry()
+            }
+        )
+        storiesViewModel.getStories(token).observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
-
-    private fun showLoading(isLoading: Boolean) {
-        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
+
+//    private fun getAllStories(data: List<ListStoryItem>) {
+//        val adapter = StoriesAdapter(data)
+//        binding.rvStory.adapter = adapter
+//        }
+//
+//    private fun showLoading(isLoading: Boolean) {
+//        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+//    }
 
     companion object {
         lateinit var sessionManager: SessionManager
---
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/model/StoriesViewModelPaging.kt
@@ -0,0 +1,33 @@
+package com.example.katahatiplus.model
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.katahatiplus.data.StoriesRepository
+import com.example.katahatiplus.injection.Injection
+import com.example.katahatiplus.response.ListStoryItem
+
+class StoriesViewModelPaging(private val storiesRepository: StoriesRepository): ViewModel() {
+
+    fun getStories(token: String) : LiveData<PagingData<ListStoryItem>> =
+        storiesRepository.getStories(token).cachedIn(viewModelScope)
+
+    companion object {
+        private const val TAG = "StoriesViewModelPaging"
+    }
+}
+
+class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(StoriesViewModelPaging::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return StoriesViewModelPaging(Injection.provideRepository(context)) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
