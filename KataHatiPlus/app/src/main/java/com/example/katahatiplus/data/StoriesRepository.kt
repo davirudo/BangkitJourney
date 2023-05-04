d81f58442b64c280d646e100885dc7ae76a8b28f
David Diamanta <dantadiamanta@gmail.com>
2023-05-04 14:24:11 +0700

    getStory

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/data/StoriesRepository.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/data/StoriesRepository.kt
@@ -1,4 +1,22 @@
 package com.example.katahatiplus.data
 
-class StoriesRepository {
+import androidx.lifecycle.LiveData
+import androidx.paging.*
+import com.example.katahatiplus.database.StoriesDatabase
+import com.example.katahatiplus.response.ListStoryItem
+import com.example.katahatiplus.retrofit.ApiService
+
+
+class StoriesRepository(private val storiesDatabase: StoriesDatabase, private val apiService: ApiService){
+
+    fun getStories(): LiveData<PagingData<ListStoryItem>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 5
+            ),
+            pagingSourceFactory = {
+                StoriesPagingSource(apiService)
+            }
+        ).liveData
+    }
 }
