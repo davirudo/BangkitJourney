cabeaf9aaf95c4ea9911b2e836be235e688e83f5
David Diamanta <dantadiamanta@gmail.com>
2023-05-04 22:14:40 +0700

    @Dao

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/database/StoriesDao.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/database/StoriesDao.kt
@@ -1,11 +1,13 @@
 package com.example.katahatiplus.database
 
 import androidx.paging.PagingSource
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.katahatiplus.response.ListStoryItem
 
+@Dao
 interface StoriesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
