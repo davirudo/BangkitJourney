653cc37ddfa232940d5372ed9a30e5a54f951280
David Diamanta <dantadiamanta@gmail.com>
2023-05-04 14:24:00 +0700

    Create StoriesDao.kt

---
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/database/StoriesDao.kt
@@ -0,0 +1,20 @@
+package com.example.katahatiplus.database
+
+import androidx.paging.PagingSource
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.katahatiplus.response.ListStoryItem
+
+interface StoriesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStories(quote: List<ListStoryItem>)
+
+    @Query("SELECT * FROM stories")
+    fun getAllStories(): PagingSource<Int, ListStoryItem>
+
+    @Query("DELETE FROM stories")
+    suspend fun deleteAll()
+
+}
