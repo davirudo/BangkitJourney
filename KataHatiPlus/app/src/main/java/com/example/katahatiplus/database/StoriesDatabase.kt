e2bc1453dfba6e2a93f037eb9dcf3e04d92984f9
David Diamanta <dantadiamanta@gmail.com>
2023-05-04 23:48:10 +0700

    remoteKeysDao

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/database/StoriesDatabase.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/database/StoriesDatabase.kt
@@ -8,12 +8,13 @@ import com.example.katahatiplus.response.ListStoryItem
 
 @Database(
     entities = [ListStoryItem::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class StoriesDatabase : RoomDatabase() {
 
     abstract fun storiesDao(): StoriesDao
+    abstract fun remoteKeysDao(): RemoteKeysDao
 
     companion object {
         @Volatile
