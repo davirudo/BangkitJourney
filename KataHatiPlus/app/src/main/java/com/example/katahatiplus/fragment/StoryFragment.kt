b3e2ac89035d697aeb96198aae3b1e6d98f38745
David Diamanta <dantadiamanta@gmail.com>
2023-05-05 09:35:07 +0700

    KEK GINI GA SI?!

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/fragment/StoryFragment.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/fragment/StoryFragment.kt
@@ -7,18 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.katahatiplus.activity.LoginActivity
 import com.example.katahatiplus.adapter.LoadingStateAdapter
-import com.example.katahatiplus.adapter.StoriesAdapter
 import com.example.katahatiplus.adapter.StoriesAdapterPaging
 import com.example.katahatiplus.databinding.FragmentStoryBinding
-import com.example.katahatiplus.model.StoriesViewModel
 import com.example.katahatiplus.model.StoriesViewModelPaging
 import com.example.katahatiplus.model.ViewModelFactory
-import com.example.katahatiplus.response.ListStoryItem
 import com.example.katahatiplus.utils.SessionManager
 
 class StoryFragment : Fragment() {
@@ -42,15 +38,6 @@ class StoryFragment : Fragment() {
         sessionManager = SessionManager(requireContext())
         val token = LoginActivity.sessionManager.getString("TOKEN")
         storiesViewModel.getStories(token.toString())
-//        storiesViewModel.getAllStories(token.toString())
-//
-//        storiesViewModel.listStory.observe(viewLifecycleOwner) {
-//            getAllStories(it)
-//        }
-//
-//        storiesViewModel.isLoading.observe(viewLifecycleOwner) {
-//            showLoading(it)
-//        }
 
         val layoutManager = LinearLayoutManager(requireContext())
         binding.rvStory.layoutManager = layoutManager
@@ -58,7 +45,6 @@ class StoryFragment : Fragment() {
         binding.rvStory.addItemDecoration(itemDecoration)
 
         getData(token.toString())
-
     }
 
     private fun getData(token: String) {
@@ -68,23 +54,13 @@ class StoryFragment : Fragment() {
                 adapter.retry()
             }
         )
-        storiesViewModel.getStories(token).observe(viewLifecycleOwner) {
+        storiesViewModel.getStories(token).observe(requireActivity()) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
     }
-
-//    private fun getAllStories(data: List<ListStoryItem>) {
-//        val adapter = StoriesAdapter(data)
-//        binding.rvStory.adapter = adapter
-//        }
-//
-//    private fun showLoading(isLoading: Boolean) {
-//        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
-//    }
 
     companion object {
         lateinit var sessionManager: SessionManager
         private lateinit var context: Context
     }
-
 }
