7c28e5ca8215d701ac167a2d330778c9ca691758
David Diamanta <dantadiamanta@gmail.com>
2023-05-09 08:52:57 +0700

    login validation

--- KataHatiPlus/app/src/main/java/com/example/katahatiplus/activity/LoginActivity.kt
+++ KataHatiPlus/app/src/main/java/com/example/katahatiplus/activity/LoginActivity.kt
@@ -41,46 +41,60 @@ class LoginActivity : AppCompatActivity() {
 
         playAnimation()
 
-        val edEmail = binding.edLoginEmail
-        val edPassword = binding.edLoginPassword
 
         binding.btnLogin.setOnClickListener {
             Toast.makeText(this, "Sign In.....", Toast.LENGTH_SHORT).show()
 
-            api.login(edEmail.text.toString(), edPassword.text.toString())
-                .enqueue(object : Callback<LoginResponse> {
-                    override fun onResponse(
-                        call: Call<LoginResponse>,
-                        response: Response<LoginResponse>
-                    ) {
-                        Log.e("SignInResponse", response.toString())
-                        val correct = response.isSuccessful
+            val edEmail = binding.edLoginEmail.text.toString()
+            val edPassword = binding.edLoginPassword.text.toString()
 
-                        if (correct) {
-                            val token = response.body()!!.loginResult.token
-                            sessionManager.saveString("TOKEN", "Bearer $token")
-                            sessionManager.saveBoolean("LOGIN_STATUS", true)
 
-                            val moveIntent = Intent(this@LoginActivity, MainActivity::class.java)
-                            startActivity(moveIntent)
-                            finish()
-                        } else {
-                            Toast.makeText(applicationContext, "Incorrect email or password", Toast.LENGTH_SHORT).show()
+            if(validate(edEmail, edPassword)) {
+                api.login(edEmail, edPassword)
+                    .enqueue(object : Callback<LoginResponse> {
+                        override fun onResponse(
+                            call: Call<LoginResponse>,
+                            response: Response<LoginResponse>
+                        ) {
+                            Log.e("SignInResponse", response.toString())
+                            val correct = response.isSuccessful
+
+                            if (correct) {
+                                val token = response.body()!!.loginResult.token
+                                sessionManager.saveString("TOKEN", "Bearer $token")
+                                sessionManager.saveBoolean("LOGIN_STATUS", true)
+
+                                val moveIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                                startActivity(moveIntent)
+                                finish()
+                            } else {
+                                Toast.makeText(applicationContext, "Incorrect email or password", Toast.LENGTH_SHORT).show()
+                            }
                         }
-                    }
 
-                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                        Log.e("SignInError", t.toString())
-                        Toast.makeText(applicationContext, "Something's wrong :(", Toast.LENGTH_SHORT).show()
-                    }
+                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                            Log.e("SignInError", t.toString())
+                            Toast.makeText(applicationContext, "Something's wrong :(", Toast.LENGTH_SHORT).show()
+                        }
 
-                })
+                    })
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Email or Password are not valid :(",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
         binding.tvSignUp.setOnClickListener {
             val moveIntent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(moveIntent)
         }
+    }
+
+    private fun validate(email: String, password: String): Boolean {
+        return password.length >= 8 && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
     private fun playAnimation() {
         ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_Y, -30f, 30f).apply {
