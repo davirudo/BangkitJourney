3db30bfe43f74b06a669a07f2211adb586f9f41a
David Diamanta <dantadiamanta@gmail.com>
2023-05-21 21:48:20 +0700

    item

--- HelloCompose/app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ HelloCompose/app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -4,13 +4,21 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Yep")
                 }
             }
         }
@@ -32,7 +40,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Logo",
+            modifier = Modifier.size(80.dp)
+        )
+        Column {
+            Text(text = "Hello $name!", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Welcome to Compose!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -40,36 +58,5 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     HelloComposeTheme {
         Greeting("Dave")
-    }
-}
-
-//Without Surface
-@Preview(
-    name = "Without Surface",
-    showSystemUi = true,
-    uiMode = UI_MODE_NIGHT_YES
-)
-@Composable
-fun GreetingPreview() {
-    HelloComposeTheme {
-        Greeting("Yeehaw")
-    }
-}
-
-//Without Surface
-@Preview(
-    name = "With Surface",
-    showSystemUi = true,
-    uiMode = UI_MODE_NIGHT_YES
-)
-@Composable
-fun GreetingSurfacePreview() {
-    HelloComposeTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            Greeting("Domo!")
-        }
     }
 }
