3e0d20d37ee654eabd6467101e999f0c6cdc3c53
David Diamanta <dantadiamanta@gmail.com>
2023-05-22 02:16:18 +0700

    animation

--- HelloCompose/app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ HelloCompose/app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.hellocompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -51,7 +56,7 @@ fun HelloJetpackComposeApp() {
     }
 }
 
-@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun HelloJetpackComposeAppPreview() {
     HelloComposeTheme {
@@ -62,22 +67,28 @@ fun HelloJetpackComposeAppPreview() {
 @Composable
 fun GreetingList(names: List<String>) {
     if (names.isNotEmpty()) {
-        Column {
-            for (name in names) {
-                Greeting(name)
+        LazyColumn {
+            items(names.size) { index ->
+                Greeting(name = names[index])
             }
         }
     } else {
-        Text("not found")
+        Box(contentAlignment = Alignment.Center) {
+            Text("No names found", fontSize = 24.sp)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    var isExpanded by remember {
-        mutableStateOf(false)
-    }
-
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if(isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Row(
         modifier = Modifier.padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
@@ -85,29 +96,19 @@ fun Greeting(name: String) {
         Image(
             painter = painterResource(R.drawable.ic_launcher_foreground),
             contentDescription = "Logo",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animatedSizeDp)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column(
-            modifier = Modifier
-                .weight(1f)
-        ) {
+        Column(modifier = Modifier.weight(1f))
+        {
             Text(text = "Hello $name!", fontSize = 24.sp, fontWeight = FontWeight.Bold)
             Text(text = "Welcome to Compose!")
         }
-        IconButton(onClick = { }) {
+        IconButton(onClick = {isExpanded = !isExpanded}) {
             Icon(
-                imageVector = if(isExpanded) Icons.Outlined.ExpandLess else Icons.Outlined.ExpandMore,
+                imageVector = if(isExpanded) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
                 contentDescription = if (isExpanded) "Show less" else "Show more"
             )
         }
     }
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    HelloComposeTheme {
-//        Greeting("Dave")
-//    }
-//}
