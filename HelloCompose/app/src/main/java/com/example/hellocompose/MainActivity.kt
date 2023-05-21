ecaa2838839e959ca314e45faa3645a94ca9cb47
David Diamanta <dantadiamanta@gmail.com>
2023-05-21 22:31:33 +0700

    compose list

--- HelloCompose/app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ HelloCompose/app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -5,47 +5,86 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
+
+private val sampleName = listOf(
+    "Joko",
+    "Chet",
+    "Doris",
+    "Eddie",
+    "Frank",
+    "Greta",
+    "Helen",
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HelloComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Yep")
-                }
+                HelloJetpackComposeApp()
             }
         }
     }
 }
 
 @Composable
+fun HelloJetpackComposeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        GreetingList(sampleName)
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun HelloJetpackComposeAppPreview() {
+    HelloComposeTheme {
+        HelloJetpackComposeApp()
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String>) {
+    if (names.isNotEmpty()) {
+        Column {
+            for (name in names) {
+                Greeting(name)
+            }
+        }
+    } else {
+        Text("not found")
+    }
+
+}
+
+@Composable
 fun Greeting(name: String) {
-    Row {
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = painterResource(R.drawable.ic_launcher_foreground),
             contentDescription = "Logo",
             modifier = Modifier.size(80.dp)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(text = "Hello $name!", fontSize = 24.sp, fontWeight = FontWeight.Bold)
             Text(text = "Welcome to Compose!")
@@ -53,10 +92,10 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HelloComposeTheme {
-        Greeting("Dave")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    HelloComposeTheme {
+//        Greeting("Dave")
+//    }
+//}
