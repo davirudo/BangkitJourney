c80466c6f45846dc9778c38a6d045b3a5cd2af75
David Diamanta <dantadiamanta@gmail.com>
2023-05-22 17:45:50 +0700

    Update Menu.kt

--- CoffeApp/app/src/main/java/com/example/coffeapp/model/Menu.kt
+++ CoffeApp/app/src/main/java/com/example/coffeapp/model/Menu.kt
@@ -1,3 +1,18 @@
 package com.example.coffeapp.model
 
-data class Menu()
+import com.example.coffeapp.R
+
+data class Menu(
+    val image: Int,
+    val title: String,
+    val price: String,
+)
+
+val dummyMenu = listOf(
+    Menu(R.drawable.menu1, "Tiramisu Coffee Milk", "Rp. 15.000"),
+    Menu(R.drawable.menu2, "Pumpkin Spice Latte", "Rp. 25.000"),
+    Menu(R.drawable.menu3, "Light Cappucino", "Rp. 20.000"),
+    Menu(R.drawable.menu4, "Choco Creamy Latte", "Rp. 18.000"),
+)
+
+val dummyBestSellerMenu = dummyMenu.shuffled()
