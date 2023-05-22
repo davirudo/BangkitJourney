7539d88f451108890b6ca766ee460306b4273f41
David Diamanta <dantadiamanta@gmail.com>
2023-05-22 23:59:09 +0700

    implement MenuRow

--- CoffeApp/app/src/main/java/com/example/coffeapp/MainActivity.kt
+++ CoffeApp/app/src/main/java/com/example/coffeapp/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -17,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.coffeapp.model.*
 import com.example.coffeapp.ui.components.CategoryItem
+import com.example.coffeapp.ui.components.MenuItem
 import com.example.coffeapp.ui.components.SearchBar
 import com.example.coffeapp.ui.components.SectionText
 import com.example.coffeapp.ui.theme.CoffeAppTheme
@@ -34,10 +37,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CoffeeApp(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(modifier = Modifier)) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -65,6 +72,22 @@ fun CategoryRow(
     ) {
         items(dummyCategory, key = { it.textCategory }) { category ->
             CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
         }
     }
 }
