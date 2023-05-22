8dcee9cfaf5e3b4631742afee5aad755a9ac97a3
David Diamanta <dantadiamanta@gmail.com>
2023-05-22 22:50:47 +0700

    implement searchBar

--- CoffeApp/app/src/main/java/com/example/coffeapp/MainActivity.kt
+++ CoffeApp/app/src/main/java/com/example/coffeapp/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.coffeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coffeapp.ui.components.SearchBar
 import com.example.coffeapp.ui.theme.CoffeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,10 +34,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CoffeeApp() {
-
+    Column {
+        Banner()
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun DefaultPreview() {
     CoffeAppTheme {
