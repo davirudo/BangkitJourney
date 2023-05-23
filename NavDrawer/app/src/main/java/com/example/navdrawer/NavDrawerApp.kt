d4bc1543f8e58914723ea35932457edfe9cd3584
David Diamanta <dantadiamanta@gmail.com>
2023-05-23 16:39:04 +0700

    home fav profile

--- NavDrawer/app/src/main/java/com/example/navdrawer/NavDrawerApp.kt
+++ NavDrawer/app/src/main/java/com/example/navdrawer/NavDrawerApp.kt
@@ -1,18 +1,24 @@
 package com.example.navdrawer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.navdrawer.ui.theme.NavDrawerTheme
 import kotlinx.coroutines.launch
 
@@ -34,12 +40,14 @@ fun NavDrawerApp() {
             )
         },
         drawerContent = {
-            Text(stringResource(R.string.hello_from_nav_drawer))
+            DrawerContent(onItemSelected = {})
         },
         drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
 ) { paddingValues ->
         Box(
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
             Text(stringResource(R.string.hello_world))
@@ -65,6 +73,60 @@ fun MyTopBar(onMenuClick: () -> Unit) {
             Text(stringResource(R.string.app_name))
         },
     )
+}
+
+data class MenuItem(val title: String, val icon: ImageVector)
+
+@Composable
+fun DrawerContent(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title: String) -> Unit,
+) {
+    val items = listOf(
+        MenuItem(
+            title = stringResource(R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+    Column(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(190.dp)
+                .background(MaterialTheme.colors.primary)
+        )
+        for (item in items) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onItemSelected(item.title)
+                    }
+                    .padding(vertical = 12.dp, horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = item.title,
+                    tint = Color.DarkGray
+                )
+                Spacer(modifier = Modifier.width(32.dp))
+                Text(
+                    text = item.title,
+                    style = MaterialTheme.typography.subtitle2,
+                )
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
