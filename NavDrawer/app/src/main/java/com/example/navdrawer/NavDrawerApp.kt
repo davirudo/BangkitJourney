3f94886b656171b42ab061695db34e7d9760d856
David Diamanta <dantadiamanta@gmail.com>
2023-05-23 16:26:45 +0700

    early open drawer

--- NavDrawer/app/src/main/java/com/example/navdrawer/NavDrawerApp.kt
+++ NavDrawer/app/src/main/java/com/example/navdrawer/NavDrawerApp.kt
@@ -7,21 +7,36 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navdrawer.ui.theme.NavDrawerTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun NavDrawerApp() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             MyTopBar(
                 onMenuClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
                 }
             )
         },
+        drawerContent = {
+            Text(stringResource(R.string.hello_from_nav_drawer))
+        },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
 ) { paddingValues ->
         Box(
             modifier = Modifier.fillMaxSize().padding(paddingValues),
