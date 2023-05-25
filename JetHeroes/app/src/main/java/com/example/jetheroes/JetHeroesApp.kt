4346fbc28c7265dbfa85adfd20acce650342f044
David Diamanta <dantadiamanta@gmail.com>
2023-05-25 15:01:35 +0700

    early code

---
+++ JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
@@ -0,0 +1,22 @@
+package com.example.jetheroes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetheroes.ui.theme.JetHeroesTheme
+
+@Composable
+fun JetHeroesApp(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun JetHeroesAppPreview() {
+    JetHeroesTheme {
+        JetHeroesApp()
+    }
+}
