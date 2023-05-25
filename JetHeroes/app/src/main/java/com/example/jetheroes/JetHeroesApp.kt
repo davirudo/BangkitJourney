110acbd2ee11438be7fe132926aab1e703ad868f
David Diamanta <dantadiamanta@gmail.com>
2023-05-25 15:44:52 +0700

    Hero list item

--- JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
+++ JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
@@ -1,9 +1,18 @@
 package com.example.jetheroes
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
@@ -13,6 +22,48 @@ fun JetHeroesApp(
     Box(modifier = modifier) {
     }
 }
+
+@Composable
+fun HeroListItem(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable {}
+    ) {
+        AsyncImage(
+            model = photoUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroListItemPreview() {
+    JetHeroesTheme {
+        HeroListItem(
+            name = "H.O.S. Cokroaminoto",
+            photoUrl = ""
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun JetHeroesAppPreview() {
