9890d9e1ee8169a1e75bea9bfe3d8fdd8d1c25ae
David Diamanta <dantadiamanta@gmail.com>
2023-05-26 15:41:35 +0700

    VMfactory

--- JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
+++ JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.jetheroes
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.jetheroes.data.HeroRepository
 import com.example.jetheroes.model.Hero
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,4 +15,15 @@ class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
             .groupBy { it.name.first() }
     )
     val groupedHeroes: StateFlow<Map<Char,List<Hero>>> get() = _groupHeroes
+}
+
+class ViewModelFactory(private val repository: HeroRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(JetHeroesViewModel::class.java)) {
+            return JetHeroesViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
 }
