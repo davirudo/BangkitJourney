44b1bbe3e776fcbda355e2f8dac04d9011133bc1
David Diamanta <dantadiamanta@gmail.com>
2023-05-26 16:02:54 +0700

    get query for search

--- JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
+++ JetHeroes/app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.jetheroes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.jetheroes.data.HeroRepository
@@ -15,6 +17,16 @@ class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
             .groupBy { it.name.first() }
     )
     val groupedHeroes: StateFlow<Map<Char,List<Hero>>> get() = _groupHeroes
+
+    private val _query = mutableStateOf("")
+    val query: State<String> get() = _query
+
+    fun search(newQuery: String) {
+        _query.value = newQuery
+        _groupHeroes.value = repository.searchHeroes(newQuery)
+            .sortedBy { it.name }
+            .groupBy { it.name[0] }
+    }
 }
 
 class ViewModelFactory(private val repository: HeroRepository) :
