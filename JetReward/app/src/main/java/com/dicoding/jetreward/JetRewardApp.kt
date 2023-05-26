2b5a4f11f1550656993dd7cc66e9dfb79b9b1607
David Diamanta <dantadiamanta@gmail.com>
2023-05-26 19:30:06 +0700

    functionable nav

--- JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -1,5 +1,6 @@
 package com.dicoding.jetreward
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -7,31 +8,60 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 import com.dicoding.jetreward.ui.navigation.Screen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.dicoding.jetreward.ui.screen.cart.CartScreen
+import com.dicoding.jetreward.ui.screen.home.HomeScreen
+import com.dicoding.jetreward.ui.screen.profile.ProfileScreen
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
 ) {
     Scaffold(
-        bottomBar = { BottomBar()
+        bottomBar = {
+            BottomBar(navController)
         },
         modifier = modifier
     ) { innerPadding ->
-
-
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+            composable(Screen.Cart.route) {
+                CartScreen()
+            }
+            composable(Screen.Profile.route) {
+                ProfileScreen()
+            }
+        }
     }
 }
 
 @Composable
 private fun BottomBar(
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     BottomNavigation(
         modifier = modifier
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
@@ -59,8 +89,16 @@ private fun BottomBar(
                         )
                     },
                     label = { Text(item.title) },
-                    selected = true,
-                    onClick = {}
+                    selected = currentRoute == item.screen.route,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
                 )
             }
         }
