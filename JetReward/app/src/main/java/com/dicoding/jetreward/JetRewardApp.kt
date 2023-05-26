e70b590dda4d4eca455c8c21fbb4a2cb64bcc104
David Diamanta <dantadiamanta@gmail.com>
2023-05-26 19:12:00 +0700

    bottomBar

--- JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -1,15 +1,66 @@
 package com.dicoding.jetreward
 
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
+import com.dicoding.jetreward.ui.navigation.Screen
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
 ) {
 
+}
+
+@Composable
+private fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_cart),
+                icon = Icons.Default.ShoppingCart,
+                screen = Screen.Cart
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.Profile
+            ),
+        )
+        BottomNavigation {
+            navigationItems.map { item ->
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title
+                        )
+                    },
+                    label = { Text(item.title) },
+                    selected = true,
+                    onClick = {}
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
