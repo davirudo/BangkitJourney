05dafb4ddaa61ba3427e8bf1bd0e5e7adcc23b46
David Diamanta <dantadiamanta@gmail.com>
2023-05-27 14:23:37 +0700

    profile

--- WarungRamen/app/src/main/java/com/example/warungramen/ui/screen/profile/ProfileScreen.kt
+++ WarungRamen/app/src/main/java/com/example/warungramen/ui/screen/profile/ProfileScreen.kt
@@ -1,12 +1,19 @@
 package com.example.warungramen.ui.screen.profile
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.warungramen.R
 
 @Composable
@@ -17,6 +24,30 @@ fun ProfileScreen(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Text(stringResource(R.string.menu_profile))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            val imageBitmap: Painter = painterResource(R.drawable.yeehaw)
+
+            Image(
+                painter = imageBitmap,
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.size(50.dp))
+            Text(
+                stringResource(R.string.nama),
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.h6.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Text(
+                stringResource(R.string.email),
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.secondary
+            )
+        }
     }
 }
