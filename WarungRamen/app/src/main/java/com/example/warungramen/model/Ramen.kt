30f01e8e4b2c2d5c6e5eecdbcbf856bd90d330ed
David Diamanta <dantadiamanta@gmail.com>
2023-05-27 09:16:27 +0700

    data

--- WarungRamen/app/src/main/java/com/example/warungramen/model/Ramen.kt
+++ WarungRamen/app/src/main/java/com/example/warungramen/model/Ramen.kt
@@ -4,5 +4,5 @@ data class Ramen(
     val id: Long,
     val image: Int,
     val title: String,
-    val requiredPoint: Int,
+    val requiredPrice: Int,
 )
--- WarungRamen/app/src/main/java/com/example/warungramen/model/RamenDataSource.kt
+++ WarungRamen/app/src/main/java/com/example/warungramen/model/RamenDataSource.kt
@@ -4,6 +4,17 @@ import com.example.warungramen.R
 
 object RamenDataSource {
     val dummyRamens = listOf(
-        Ramen(1, R.drawable.curry, "mmm Yummy", 5000),
+        Ramen(1, R.drawable.gyudon, "Gyudon", 40000),
+        Ramen(2, R.drawable.yakiniku, "Beef Yakiniku Bowl", 47000),
+        Ramen(3, R.drawable.curry, "Chicken Katsu Curry", 47000),
+        Ramen(4, R.drawable.katsu, "Chicken Katsu Bowl", 37000),
+        Ramen(5, R.drawable.ramentokyo, "Tokyo Ramen", 50000),
+        Ramen(6, R.drawable.ramenjakarta, "Jakarta Ramen", 50000),
+        Ramen(7, R.drawable.mozzarela, "Fried Mozzarella Stick", 25000),
+        Ramen(8, R.drawable.nasiayam, "Rice Fried Chicken", 25000),
+        Ramen(9, R.drawable.matcha, "Brown Sugar Mactha Latte", 30000),
+        Ramen(10, R.drawable.mineral, "Mineral Water", 7000),
+
+
     )
 }
