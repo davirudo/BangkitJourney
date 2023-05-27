a41a80b15f3728cd190af32b9bff34cc466a4f43
David Diamanta <dantadiamanta@gmail.com>
2023-05-27 14:46:27 +0700

    theme

--- WarungRamen/app/src/main/java/com/example/warungramen/ui/theme/Color.kt
+++ WarungRamen/app/src/main/java/com/example/warungramen/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.warungramen.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red200 = Color(0xFFEDC3C3)
+val Red500 = Color(0xFF502D2D)
+val Red700 = Color(0xFF391515)
+val Magenta500 = Color(0xFFC61165)
